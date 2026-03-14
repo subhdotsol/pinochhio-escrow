2f466598820c020fcc36cd8e3fa5d9b1ce2e2463
subh <subhajitchaudhury05@gmail.com>
2026-03-14 21:28:34 +0530

    feat: added processor

--- src/processor.rs
+++ src/processor.rs
@@ -1,15 +1,19 @@
 use pinocchio::{AccountView, Address, ProgramResult};
+use crate::instructions::EscrowInstruction;
 
-pub struct Processor;
+pub fn process_instruction(
+    program_id: &Address,
+    accounts: &[AccountView],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let instruction = EscrowInstruction::unpack(instruction_data)?;
 
-impl Processor {
-    pub fn process(
-        program_id: &Address,
-        accounts: &mut [AccountView],
-        instruction_data: &[u8],
-    ) -> ProgramResult {
-        let instruction = EscrowInstruction::unpack(data)?;
-
-        instruction.process(program_id, accounts)
+    match instruction {
+        EscrowInstruction::Make { amount_a, amount_b } => {
+            crate::instructions::make::make(program_id, accounts, amount_a, amount_b)
+        }
+        EscrowInstruction::Take { amount_a, amount_b } => {
+            crate::instructions::take::take(program_id, accounts, amount_a, amount_b)
+        }
     }
 }
